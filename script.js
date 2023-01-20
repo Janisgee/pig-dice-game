5c2f86d12fe1420243f89fdac005343b8bd88380
Janisgee <100589152+Janisgee@users.noreply.github.com>
2023-01-20 16:28:51 +0800

    Added random dice roll in Js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const btnRollEl = document.querySelector(".btn-rollDice");
+const btnNewEl = document.querySelector(".btn-new");
+const btnHoldEl = document.querySelector(".btn-hold");
+
+const diceImageEl = document.querySelector(".dice-image");
+
+// Starting condition
+diceImageEl.classList.add("imageHidden");
+
+// When pressed Roll Dice Button
+btnRollEl.addEventListener("click", function() {
+    diceImageEl.classList.remove("imageHidden");
+    //Generate random dice roll
+    let dice = Math.trunc(Math.random() * 6) + 1;
+    //Display dice roll
+    diceImageEl.src = `dice-${dice}.png`;
+});
